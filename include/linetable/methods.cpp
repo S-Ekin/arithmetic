0eb1975a61d24260986313186fcf5f284f6a2dad
S-Ekin <sekin48111@gmail.com>
2022-08-07 22:36:17 +0800

    一、求差集 	1、自己是用二层while 实现的，但是答案可以之间用一层while 二、划分分界线算法

--- include/linetable/methods.cpp
+++ include/linetable/methods.cpp
@@ -6,6 +6,8 @@
  */
 #include <stdlib.h>
 #include <iostream>
+#include "./linetable.h"
+#include "./singleLink.cpp"
 using namespace std;
 
 //逆置数组
@@ -15,6 +17,17 @@ void cinOutArr(int arr[]){
 	 {
 		cout << arr[i] << " , " ;
 	 }
+	cout << endl;
+}
+
+void cinOutArr(LNode * a){
+	a = a->next;
+	while (a !=NULL)
+	{
+		cout << a->data << " , ";
+		a = a->next;
+	}
+	
 	cout << endl;
 }
 void  reserveArrAndChange(int arr[], int k, int n){
@@ -46,9 +59,88 @@ void  reserveArrAndChange(int arr[], int k, int n){
 	cinOutArr(arr);
 }
 
+
+// 划分
+void partition(int arr[], int n){
+	int i, j, temp;
+	i = 0; 
+	j = n - 1;
+	temp = arr[0];
+
+	while (i<j)
+	{
+		//先从右边j开始扫描，
+		while (i< j && arr[j] >= temp) { j--; }
+		// 腾出位置j
+		if(i<j){
+			arr[i] = arr[j];
+			i++;
+		}
+		// 再扫描i
+		while (i< j && arr[i] < temp) { i++; }
+		// 腾出位置i
+	   if(i<j){
+			arr[j] = arr[i];
+			j--;
+		}
+		
+	}
+	// 最后就是temp 的位置
+	arr[i] = temp;
+	
+
+}
+
+// 归并
+void mergearray(){
+
+}
+
+// 差集
+void subtraction(LNode *&A, LNode*B){
+	LNode *a,*b,*r;
+	a = A;
+	b = B;
+	cinOutArr(a);
+	cinOutArr(b);
+
+	a=a->next;
+	b=b->next;
+
+	A->next = NULL;
+	r = A;
+
+	free(B);
+	while (a!=NULL)
+	{
+		while (b !=NULL && b->data < a->data)
+		{
+			b = b->next;
+		}
+		if(b == NULL || a == NULL){
+			break;
+		}
+		if(b->data == a->data){
+			r->next = a;
+			r = r->next;
+			b=b->next;
+		} 
+		a = a->next;
+	}
+	r->next = NULL; //记得断开和之前的连接
+}
+
+void testSubtraction(){
+	int A[5] = {1,3,5,9,10};
+	int B[8] = {-1,0,3,5,6,7,9,10};
+	LNode *a, *b;
+	createListR(a, A, 5);
+	createListR(b, B, 8);
+	subtraction(a,b);
+	cinOutArr(a);
+}
+
 int main(){
-	int arr [7] = {1,2,3,4,5,6,7};
-	reserveArrAndChange(arr, 5, 7);
-	cinOutArr(arr);
+	testSubtraction();
 	return 0;
 }
